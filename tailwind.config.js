82acb10ab19033c891cfe75bdcbf51f89c00b483
sultanalif01 <sultanalif0310@gmail.com>
2022-10-30 16:36:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary : '#99c6d8',
+                header : "#F6F9FC",
+            },
+            fontFamily: {
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
+            },
+        }
+    }
+}
